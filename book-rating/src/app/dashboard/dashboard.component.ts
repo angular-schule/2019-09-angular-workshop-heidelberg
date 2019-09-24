dac753ad58845f8342b8641eeaef5af3e0e17456
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-09-24 16:03:08 +0200

    introducing BookComponent

--- book-rating/src/app/dashboard/dashboard.component.ts
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../shared/book';
 
 @Component({
   selector: 'br-dashboard',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  books: string[];
+  books: Book[];
 
   ngOnInit() {
-    this.books = ['AngularJS', 'Angular', 'React'];
+    this.books = [
+      {
+        isbn: '111',
+        title: 'Angular',
+        description: 'Tolles Buch',
+        rating: 5
+      },
+      {
+        isbn: '222',
+        title: 'AngularJS',
+        description: 'Altes Buch',
+        rating: 5
+      },
+      {
+        isbn: '333',
+        title: 'React',
+        description: 'Mehhh....',
+        rating: 3
+      }
+
+    ];
   }
 }
