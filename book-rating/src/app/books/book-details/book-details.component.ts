52eff95b784228f9d528bfd286aa3123a3e73b70
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-09-27 12:10:48 +0200

    rxjs: higher order, anti pattern

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,57 +1,31 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { map, filter, scan, reduce } from 'rxjs/operators';
+import { map, filter, scan, reduce, tap } from 'rxjs/operators';
 import { of, from, timer, interval, Subscription, Observable } from 'rxjs';
+import { BookStoreService } from '../shared/book-store.service';
+import { Book } from '../shared/book';
 
 @Component({
   selector: 'br-book-details',
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.scss']
 })
-export class BookDetailsComponent implements OnInit, OnDestroy {
+export class BookDetailsComponent implements OnInit {
 
-  isbn: string;
+  book: Book;
   subscription: Subscription;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private bs: BookStoreService) { }
 
   ngOnInit() {
-    // #region
-    // only for simple cases
-    // this.isbn = this.route.snapshot.paramMap.get('isbn');
 
     this.route.paramMap
       .pipe(
-        map(paramMap => paramMap.get('isbn'))
+        map(paramMap => paramMap.get('isbn')),
+        map(isbn => this.bs.getSingle(isbn)),
       )
-      .subscribe(isbn => this.isbn = isbn);
-      // #endregion
-
-
-    /// --- PLAYGROUND ----
-
-    const observer = {
-      next: s => console.log(s),
-      error: e => console.error(e),
-      complete: () => console.log('COMPLETE')
-    };
-
-    const observable = from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-      .pipe(
-        map(z => z * 10),
-        filter(z => z > 20),
-        map(z => z + 5),
-        reduce((a, b) => a + b),
-        map(summe => '⭐️'.repeat(summe))
-      );
-
-    this.subscription = observable
-        .subscribe(observer);
-  }
-
-  // beste Alternative: async pipe
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
+      .subscribe(book$ => book$
+          .subscribe(book => this.book = book));
   }
 
 }
